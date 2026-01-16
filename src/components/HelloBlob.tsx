41e4390f42e455b5bd803f06b510f11c30303e65
dickson <tlau@alnylam.com>
2026-01-16 17:16:16 -0500

    Optimize hello blob

--- src/components/HelloBlob.tsx
+++ src/components/HelloBlob.tsx
@@ -3,132 +3,134 @@ import * as React from 'react';
 import styled from '@emotion/styled';
 
 interface HelloBlobProps {
-  morphTime: number;
-  cooldownTime: number;
-  strings: string[];
+    morphTime: number;
+    cooldownTime: number;
+    strings: string[];
 }
 
 const BlobContainer = styled.div`
-  filter: url(#threshold) blur(0.6px);
-  position: relative;
-  white-space: pre;
-  height: 80px;
+    filter: url(#threshold) blur(0.6px);
+    position: relative;
+    white-space: pre;
+    height: 80px;
 `;
 
 const HelloWord = styled.span`
-  position: absolute;
-  font-size: 1.3em;
-  user-select: none;
+    position: absolute;
+    font-size: 1.3em;
+    user-select: none;
 `;
 
 const InkBlob = styled.svg`
-  height: 1px;
+    height: 1px;
 `;
 
 export const HelloBlob = ({ morphTime, cooldownTime, strings }: HelloBlobProps) => {
+    const textIndex = React.useRef(strings.length - 1);
+    const time = React.useRef(new Date().getTime());
+    const morph = React.useRef(0);
+    const cooldown = React.useRef(cooldownTime);
 
-  const textIndex = React.useRef(strings.length - 1);
-  const time = React.useRef(new Date().getTime());
-  const morph = React.useRef(0);
-  const cooldown = React.useRef(cooldownTime);
+    const wordRef1 = React.useRef<HTMLSpanElement>(null);
+    const wordRef2 = React.useRef<HTMLSpanElement>(null);
 
-  const wordRef1 = React.useRef<HTMLSpanElement>(null);
-  const wordRef2 = React.useRef<HTMLSpanElement>(null);
+    const requestRef = React.useRef(0);
 
-  const requestRef = React.useRef(0);
-
-  if (wordRef1.current && wordRef2.current) {
-    wordRef1.current.textContent = strings[textIndex.current % strings.length];
-    wordRef2.current.textContent = strings[(textIndex.current + 1) % strings.length];
-  }
-
-  function doMorph() {
-    morph.current -= cooldown.current;
-    cooldown.current = 0;
-
-    let fraction = morph.current / morphTime;
-
-    if (fraction > 1) {
-        cooldown.current = cooldownTime;
-        fraction = 1;
-    }
-
-    setMorph(fraction);
-  }
-
-  function setMorph(fraction: number) {
     if (wordRef1.current && wordRef2.current) {
-      wordRef2.current.style.filter = `blur(${Math.min(3 / fraction - 3, 100)}px)`;
-
-      wordRef2.current.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-
-      fraction = 1 - fraction;
-
-      wordRef1.current.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-      wordRef1.current.style.opacity = `${Math.pow(fraction, 0.2) * 100}%`;
-
-      wordRef1.current.textContent = strings[textIndex.current % strings.length];
-      wordRef2.current.textContent = strings[(textIndex.current + 1) % strings.length];
-    }
-  }
-
-  function doCooldown() {
-    morph.current = 0;
-    if (wordRef1.current && wordRef2.current) {
-      wordRef2.current.style.filter = '';
-      wordRef2.current.style.opacity = '100%';
-
-      wordRef1.current.style.filter = '';
-      wordRef1.current.style.opacity = '0%';
-    }
-  }
-
-
-  const animate = () => {
-    if (textIndex.current < (strings.length * 2) - 1) {
-      requestAnimationFrame(animate);
+        wordRef1.current.textContent = strings[textIndex.current % strings.length];
+        wordRef2.current.textContent = strings[(textIndex.current + 1) % strings.length];
     }
 
-    const newTime = new Date();
-    const shouldIncrementIndex = cooldown.current > 0;
-    const dt = (newTime.getTime() - time.current) / 1000;
-    time.current = newTime.getTime();
+    React.useEffect(() => {
+        function doMorph() {
+            morph.current -= cooldown.current;
+            cooldown.current = 0;
 
-    cooldown.current -= dt;
+            let fraction = morph.current / morphTime;
 
-    if (cooldown.current <= 0) {
-        if (shouldIncrementIndex) {
-          textIndex.current++;
+            if (fraction > 1) {
+                cooldown.current = cooldownTime;
+                fraction = 1;
+            }
+
+            setMorph(fraction);
         }
 
-        doMorph();
-    } else {
-        doCooldown();
-    }
-  };
+        function setMorph(fraction: number) {
+            if (wordRef1.current && wordRef2.current) {
+                wordRef2.current.style.filter = `blur(${Math.min(3 / fraction - 3, 100)}px)`;
 
-  React.useEffect(() => {
-    requestRef.current = requestAnimationFrame(animate);
-    return () => cancelAnimationFrame(requestRef.current);
-  }, []);
+                wordRef2.current.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
 
-  return (
-    <>
-      <BlobContainer>
-        <HelloWord ref={wordRef1}/>
-        <HelloWord ref={wordRef2} />
-      </BlobContainer>
+                fraction = 1 - fraction;
 
-      <InkBlob id="filters">
-        <defs>
-          <filter id="threshold">
-              <feColorMatrix in="SourceGraphic" type="matrix" values="1 0 0 0 0
+                wordRef1.current.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+                wordRef1.current.style.opacity = `${Math.pow(fraction, 0.2) * 100}%`;
+
+                wordRef1.current.textContent = strings[textIndex.current % strings.length];
+                wordRef2.current.textContent = strings[(textIndex.current + 1) % strings.length];
+            }
+        }
+
+        function doCooldown() {
+            morph.current = 0;
+            if (wordRef1.current && wordRef2.current) {
+                wordRef2.current.style.filter = '';
+                wordRef2.current.style.opacity = '100%';
+
+                wordRef1.current.style.filter = '';
+                wordRef1.current.style.opacity = '0%';
+            }
+        }
+
+        const animate = () => {
+            if (textIndex.current < strings.length * 2 - 1) {
+                requestRef.current = requestAnimationFrame(animate);
+            }
+
+            const newTime = new Date();
+            const shouldIncrementIndex = cooldown.current > 0;
+            const dt = (newTime.getTime() - time.current) / 1000;
+            time.current = newTime.getTime();
+
+            cooldown.current -= dt;
+
+            if (cooldown.current <= 0) {
+                if (shouldIncrementIndex) {
+                    textIndex.current++;
+                }
+
+                doMorph();
+            } else {
+                doCooldown();
+            }
+        };
+
+        requestRef.current = requestAnimationFrame(animate);
+        return () => cancelAnimationFrame(requestRef.current);
+    }, [strings, cooldownTime, morphTime]);
+
+    return (
+        <>
+            <BlobContainer style={{ willChange: 'filter' }}>
+                <HelloWord ref={wordRef1} />
+                <HelloWord ref={wordRef2} />
+            </BlobContainer>
+
+            <InkBlob id="filters">
+                <defs>
+                    <filter id="threshold">
+                        <feColorMatrix
+                            in="SourceGraphic"
+                            type="matrix"
+                            values="1 0 0 0 0
                     0 1 0 0 0
                     0 0 1 0 0
-                    0 0 0 255 -140" />
-          </filter>
-        </defs>
-      </InkBlob>
-    </>
-  );
+                    0 0 0 255 -140"
+                        />
+                    </filter>
+                </defs>
+            </InkBlob>
+        </>
+    );
 };
--- src/components/Pages.tsx
+++ src/components/Pages.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import styled from '@emotion/styled';
-import { AiFillLinkedin, AiFillGithub, AiFillCloud, AiFillMail, AiOutlinePaperClip } from 'react-icons/ai';
+import { AiFillLinkedin, AiFillGithub, AiFillMail, AiOutlinePaperClip } from 'react-icons/ai';
 import { HelloBlob } from './HelloBlob';
 import headshot from '../images/about.jpg';
 import { Bubbles, Shine, Headshot, Twirl, Chomp, LetterPop, Dang, Typing, FocusLens } from './Animation';
@@ -9,242 +9,267 @@ import { useTheme } from '@emotion/react';
 import { Automagick } from './Automagick';
 
 const AboutText = styled(BlackText)`
-  display: flex;
+    display: flex;
 `;
 
+const helloStrings: string[] = [
+    ' ',
+    'Hello!',
+    '你好!',
+    'Salut!',
+    '안녕!',
+    'Bonjour!',
+    'привет!',
+    'Hola!',
+    'नमस्ते!',
+    "What's good?",
+];
+
 export const About = () => {
-  const [appear, setAppear] = React.useState(false);
-  const [headshotLoad, setHeadshotLoad] = React.useState(false);
-  const [popOut, setPopOut] = React.useState(false);
-  const [popIn, setPopIn] = React.useState(false);
+    const [appear, setAppear] = React.useState(false);
+    const [headshotLoad, setHeadshotLoad] = React.useState(false);
+    const [popOut, setPopOut] = React.useState(false);
+    const [popIn, setPopIn] = React.useState(false);
 
-  const helloStrings: string[] =[
-    " ",
-    "Hello!",
-    "你好!",
-    "Salut!",
-    "안녕!",
-    "Bonjour!",
-    "привет!",
-    "Hola!",
-    "नमस्ते!",
-    "What's good?"
-  ];
+    React.useLayoutEffect(() => {
+        const timer = setTimeout(() => {
+            setAppear(true);
+        }, 200);
 
-  React.useLayoutEffect(() => {
-    const timer = setTimeout(() => {
-      setAppear(true);
-    }, 200);
+        return () => clearTimeout(timer);
+    }, []);
 
-    return () => clearTimeout(timer);
-  }, []);
+    React.useLayoutEffect(() => {
+        const timer = setTimeout(() => {
+            setHeadshotLoad(true);
+        }, 2500);
 
-  React.useLayoutEffect(() => {
-    const timer = setTimeout(() => {
-      setHeadshotLoad(true);
-    }, 2500);
+        return () => clearTimeout(timer);
+    }, []);
 
-    return () => clearTimeout(timer);
-  }, []);
+    const pageId = 'about';
 
-  const pageId = 'about';
-
-  return (
-    <Page pageId={ pageId } key={ `${pageId}_pagekey` }>
-      <Headshot
-        img={ headshot }
-        appear={ appear }
-        popOut={ popOut }
-        popIn={ popIn }
-        onMouseEnter={ () => {setPopOut(true); setPopIn(false);}}
-        onMouseLeave={ () => {setPopOut(false); setPopIn(true);}}
-        className={ headshotLoad ? 'loaded' : ''}
-      />
-      <AboutText>
-        <HelloBlob
-          morphTime={ 1 }
-          cooldownTime={ .75 }
-          strings={ helloStrings }
-        />
-        My name is Dickson Lau and I&#39;m a software developer from the proud city of Boston.
-      </AboutText>
-    </Page>
-  );
+    return (
+        <Page pageId={pageId} key={`${pageId}_pagekey`}>
+            <Headshot
+                img={headshot}
+                appear={appear}
+                popOut={popOut}
+                popIn={popIn}
+                onMouseEnter={() => {
+                    setPopOut(true);
+                    setPopIn(false);
+                }}
+                onMouseLeave={() => {
+                    setPopOut(false);
+                    setPopIn(true);
+                }}
+                className={headshotLoad ? 'loaded' : ''}
+            />
+            <AboutText>
+                <HelloBlob morphTime={1} cooldownTime={0.75} strings={helloStrings} />
+                My name is Dickson Lau and I&#39;m a software developer from the proud city of Boston.
+            </AboutText>
+        </Page>
+    );
 };
 
 export const Alnylam = () => {
-  const pageId = 'alnylam';
+    const pageId = 'alnylam';
 
-  return (
-    <Page pageId={ pageId } key={ `${pageId}_page` }>
-      <BlackText className='right'>
-        I&#39;ve made a return to biotech, working at <Twirl link='http://alnylam.com' word='Alnylam' /> to build internal tools for research and development.
-      </BlackText>
-      <Logo src={ process.env.PUBLIC_URL + '/images/logos/Alnylam.png' } alt="Alnylam" />
-    </Page>
-  );
+    return (
+        <Page pageId={pageId} key={`${pageId}_page`}>
+            <BlackText className="right">
+                I&#39;ve made a return to biotech, working at <Twirl link="http://alnylam.com" word="Alnylam" /> to
+                build internal tools for research and development.
+            </BlackText>
+            <Logo src={process.env.PUBLIC_URL + '/images/logos/Alnylam.png'} alt="Alnylam" />
+        </Page>
+    );
 };
 
 export const RGG = () => {
-  const pageId = 'rgg';
+    const pageId = 'rgg';
 
-  return (
-    <Page pageId={ pageId } key={ `${pageId}_page` }>
-      <Logo src={ process.env.PUBLIC_URL + '/images/logos/RueGiltGroupe.png' } alt='RGG' />
-      <WhiteText className='left'>
-        Before that I was with <Shine href='http://www.ruegiltgroupe.com/'>Rue Gilt Groupe</Shine> cranking out countless of features for <Bubbles link='https://www.ruelala.com/' word='Rue La La' color='#FF3065' bubbleColor='#D64066B3'/> and <Bubbles link='https://www.gilt.com/' word='Gilt' color='#FFB400' bubbleColor='#E1B95BB3'/>
-      </WhiteText>
-    </Page>
-  );
+    return (
+        <Page pageId={pageId} key={`${pageId}_page`}>
+            <Logo src={process.env.PUBLIC_URL + '/images/logos/RueGiltGroupe.png'} alt="RGG" />
+            <WhiteText className="left">
+                Before that I was with <Shine href="http://www.ruegiltgroupe.com/">Rue Gilt Groupe</Shine> cranking out
+                countless of features for{' '}
+                <Bubbles link="https://www.ruelala.com/" word="Rue La La" color="#FF3065" bubbleColor="#D64066B3" /> and{' '}
+                <Bubbles link="https://www.gilt.com/" word="Gilt" color="#FFB400" bubbleColor="#E1B95BB3" />
+            </WhiteText>
+        </Page>
+    );
 };
 
 export const Grubhub = () => {
-  const pageId = 'grubhub';
-  const theme = useTheme();
+    const pageId = 'grubhub';
+    const theme = useTheme();
 
-  return (
-    <Page pageId={ pageId } key={ `${pageId}_page` }>
-      <BlackText className='right'>
-        And even prior to that, I was a part of <Chomp link='https://www.grubhub.com' word='Grubhub' color={ theme.colors.pages.grubhub }/> for a brief time during an acquisition of a smaller company.
-      </BlackText>
-    <Logo src={ process.env.PUBLIC_URL + '/images/logos/Grubhub.png' } alt='Grubhub' />
-    </Page>
-  );
+    return (
+        <Page pageId={pageId} key={`${pageId}_page`}>
+            <BlackText className="right">
+                And even prior to that, I was a part of{' '}
+                <Chomp link="https://www.grubhub.com" word="Grubhub" color={theme.colors.pages.grubhub} /> for a brief
+                time during an acquisition of a smaller company.
+            </BlackText>
+            <Logo src={process.env.PUBLIC_URL + '/images/logos/Grubhub.png'} alt="Grubhub" />
+        </Page>
+    );
 };
 
 export const LevelUp = () => {
-  const pageId ='levelUp';
+    const pageId = 'levelUp';
 
-  return (
-    <Page pageId={ pageId } key={ `${pageId}_page` }>
-      <Logo src={ process.env.PUBLIC_URL + '/images/logos/LevelUp.png' } alt='LevelUp' />
-      <WhiteText className='left'>
-        That mighty company was called <LetterPop link='https://www.thelevelup.com/' word='LevelUp' /> where I spent a good amount of time writing Ruby on Rails.
-      </WhiteText>
-    </Page>
-  );
+    return (
+        <Page pageId={pageId} key={`${pageId}_page`}>
+            <Logo src={process.env.PUBLIC_URL + '/images/logos/LevelUp.png'} alt="LevelUp" />
+            <WhiteText className="left">
+                That mighty company was called <LetterPop link="https://www.thelevelup.com/" word="LevelUp" /> where I
+                spent a good amount of time writing Ruby on Rails.
+            </WhiteText>
+        </Page>
+    );
 };
 
 export const Amalgam = () => {
-  const pageId = 'amalgam';
-  const theme = useTheme();
-  return (
-    <Page pageId={ pageId } key={ `${pageId}_page` }>
-      <BlackText className='right'>
-        Many, many summers ago, I had a brief stint at <Dang link='http://amalgam.co' word='Amalgam' color={ theme.colors.pages.amalgam }/> constructing websites from concepts.
-      </BlackText>
-      <Logo src={ process.env.PUBLIC_URL + '/images/logos/Amalgam.png' } alt='Amalgam' />
-    </Page>
-  );
+    const pageId = 'amalgam';
+    const theme = useTheme();
+    return (
+        <Page pageId={pageId} key={`${pageId}_page`}>
+            <BlackText className="right">
+                Many, many summers ago, I had a brief stint at{' '}
+                <Dang link="http://amalgam.co" word="Amalgam" color={theme.colors.pages.amalgam} /> constructing
+                websites from concepts.
+            </BlackText>
+            <Logo src={process.env.PUBLIC_URL + '/images/logos/Amalgam.png'} alt="Amalgam" />
+        </Page>
+    );
 };
 
 export const Launch = () => {
-  const pageId = 'launch';
+    const pageId = 'launch';
 
-  return (
-    <Page pageId={ pageId } key={ `${pageId}_page` }>
-      <Logo src={ process.env.PUBLIC_URL + '/images/logos/Launch.png' } alt='Launch' />
-      <WhiteText className='left'>
-        But really, this all started with the incredible experience at the <Typing link='https://www.launchacademy.com' word='coding bootcamp' /> that I attended.
-      </WhiteText>
-    </Page>
-  );
+    return (
+        <Page pageId={pageId} key={`${pageId}_page`}>
+            <Logo src={process.env.PUBLIC_URL + '/images/logos/Launch.png'} alt="Launch" />
+            <WhiteText className="left">
+                But really, this all started with the incredible experience at the{' '}
+                <Typing link="https://www.launchacademy.com" word="coding bootcamp" /> that I attended.
+            </WhiteText>
+        </Page>
+    );
 };
 
 export const Photography = () => {
-  const pageId = 'photography';
+    const pageId = 'photography';
 
-  return (
-    <Page pageId={ pageId } key={ `${pageId}_page` }>
-      <Automagick />
-      <WhiteText className='right text-shadow'>
-      I love to wander and travel. Sometimes I&#39;ll take <FocusLens href='https://www.flickr.com/photos/mrsushiman/' target='_blank' rel='noreferrer'>pictures</FocusLens> of people, places, and things.
-      </WhiteText>
-    </Page>
-  );
+    return (
+        <Page pageId={pageId} key={`${pageId}_page`}>
+            <Automagick />
+            <WhiteText className="right text-shadow">
+                I love to wander and travel. Sometimes I&#39;ll take{' '}
+                <FocusLens href="https://www.flickr.com/photos/mrsushiman/" target="_blank" rel="noreferrer">
+                    pictures
+                </FocusLens>{' '}
+                of people, places, and things.
+            </WhiteText>
+        </Page>
+    );
 };
 
 export const Contact = () => {
-  const pageId = 'contact';
+    const pageId = 'contact';
 
-  return (
-    <Page pageId={ pageId } key={ `${pageId}_page` }>
-      <ContactContainer>
-        <ContactLabel title='github' link='https://github.com/laudickson'><AiFillGithub /></ContactLabel>
-        <ContactLabel title='linkedin' link='https://linkedin.com/in/laudickson'><AiFillLinkedin /></ContactLabel>
-        {/* <ContactLabel title='Blog'><AiFillCloud /></ContactLabel> */}
-        <ContactLabel title='email' link='mailto:kr.d.tsl@gmail.com'><AiFillMail /></ContactLabel>
-        <ContactLabel title='resume' link='/resume.pdf'><AiOutlinePaperClip /></ContactLabel>
-      </ContactContainer>
-    </Page>
-  );
+    return (
+        <Page pageId={pageId} key={`${pageId}_page`}>
+            <ContactContainer>
+                <ContactLabel title="github" link="https://github.com/laudickson">
+                    <AiFillGithub />
+                </ContactLabel>
+                <ContactLabel title="linkedin" link="https://linkedin.com/in/laudickson">
+                    <AiFillLinkedin />
+                </ContactLabel>
+                {/* <ContactLabel title='Blog'><AiFillCloud /></ContactLabel> */}
+                <ContactLabel title="email" link="mailto:kr.d.tsl@gmail.com">
+                    <AiFillMail />
+                </ContactLabel>
+                <ContactLabel title="resume" link="/resume.pdf">
+                    <AiOutlinePaperClip />
+                </ContactLabel>
+            </ContactContainer>
+        </Page>
+    );
 };
 
 const ContactContainer = styled.div`
-  display: flex;
-  width: 50%;
-  justify-content: space-between;
-  @media screen and (max-width:500px) {
-    flex-direction: column;
-    gap: 36px;
-  }
-
-  a {
-    position: relative;
     display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 4px;
-    border-radius: 4px;
-
-    &::after{
-      content: "";
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      transform: scaleX(0);
-      z-index: 1;
-      transform-origin: bottom center;
-      background: white;
-      transition: 0.2s ease-out;
-      border-radius: 4px;
+    width: 50%;
+    justify-content: space-between;
+    @media screen and (max-width: 500px) {
+        flex-direction: column;
+        gap: 36px;
     }
 
-    &:hover{
-      span, svg {
-        color: black;
-        transition: 0.4s ease-out;
-      }
+    a {
+        position: relative;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 4px;
+        border-radius: 4px;
 
-      &::after{
-        transform: scaleX(1);
-      }
+        &::after {
+            content: '';
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            transform: scaleX(0);
+            z-index: 1;
+            transform-origin: bottom center;
+            background: white;
+            transition: 0.2s ease-out;
+            border-radius: 4px;
+        }
+
+        &:hover {
+            span,
+            svg {
+                color: black;
+                transition: 0.4s ease-out;
+            }
+
+            &::after {
+                transform: scaleX(1);
+            }
+        }
+
+        span,
+        svg {
+            color: white;
+            z-index: 2;
+        }
+
+        span {
+            padding-left: 4px;
+            font-size: calc(20px + 0.08vw);
+        }
+
+        svg {
+            font-size: 24px;
+        }
     }
-
-    span, svg {
-      color: white;
-      z-index: 2;
-    }
-
-    span {
-      padding-left: 4px;
-      font-size: calc(20px + 0.08vw);
-    }
-
-    svg {
-      font-size: 24px;
-    }
-  }
-
 `;
 
-const ContactLabel = ({ children, title, link }: { children: any, title: string, link: string }) => {
-  return (
-    <a href={link} target='_blank' rel="noreferrer">
-      { children }
-      <span>{ title }</span>
-    </a>
-  );
+const ContactLabel = ({ children, title, link }: { children: React.ReactNode; title: string; link: string }) => {
+    return (
+        <a href={link} target="_blank" rel="noreferrer">
+            {children}
+            <span>{title}</span>
+        </a>
+    );
 };
